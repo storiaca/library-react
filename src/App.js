ce5fd94d402a273f6eee593df4622efd7ff09658
storiaca <storiaca@gmail.com>
2019-10-27 12:03:56 +0100

    Data import fix

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./logo.svg";
+import genres from "./data/genres.js";
+import "./App.css";
 
 function App() {
+  console.log(genres);
   return (
     <div className="App">
       <header className="App-header">
