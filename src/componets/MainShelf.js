7f2f717030c663ec2e54c6891c71ac9f3f1f793c
Aleksandar Ristic <storiaca@gmail.com>
2019-12-23 16:42:51 +0100

    Step type button

--- src/componets/MainShelf.js
+++ src/componets/MainShelf.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import data from "../data/data.json";
 
 import AddSubgenre from "./AddSubgenre/AddSubgenre";
@@ -8,18 +8,33 @@ import Genre from "./Genre/Genre";
 import Button from "./UI/Button/Button";
 import classes from "./MainShelf.module.css";
 const MainShelf = props => {
+  const [step, setStep] = useState(1);
+
   const books = data.genres.map(item => {
     return <li key={item.id}>{item.name}</li>;
   });
+
+  console.log(step);
+  const nextStepHandler = e => {
+    console.log("Next");
+  };
+
+  const prevStepHandler = e => {
+    console.log("Prev");
+  };
+
   return (
     <div className={classes.MainShelf}>
       <h1>Add book - New Book</h1>
-      <Genre />
-      <SubGenre />
-      <AddSubgenre />
-      <Information />
-      <Button name={"Next"} />
-      <Button name={"Previous"} />
+      <div style={{ display: "flex", justifyContent: "center" }}>
+        <Genre />
+        <SubGenre />
+        <AddSubgenre />
+        <Information />
+      </div>
+
+      <Button name={"Previous"} prevStep={prevStepHandler} />
+      <Button name={"Next"} nextStep={nextStepHandler} />
       {books}
     </div>
   );
--- src/componets/UI/Button/Button.js
+++ src/componets/UI/Button/Button.js
@@ -1,10 +1,18 @@
 import React from "react";
 import classes from "./Button.module.css";
 
-const Button = ({ name }) => {
+const Button = ({ name, nextStep, prevStep }) => {
+  let stepType;
+  if (name === "Next") {
+    stepType = nextStep;
+  } else {
+    stepType = prevStep;
+  }
   return (
     <>
-      <button className={classes.Button}>{name}</button>
+      <button className={classes.Button} onClick={stepType}>
+        {name}
+      </button>
     </>
   );
 };
