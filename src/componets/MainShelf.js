12faf0e7efe64a02594c52192dc1c0c0a9b7a97e
Aleksandar Ristic <storiaca@gmail.com>
2019-12-24 16:12:57 +0100

    Adding and subtractoin of state

--- src/componets/MainShelf.js
+++ src/componets/MainShelf.js
@@ -8,25 +8,35 @@ import Genre from "./Genre/Genre";
 import Button from "./UI/Button/Button";
 import classes from "./MainShelf.module.css";
 const MainShelf = props => {
-  const [step, setStep] = useState(1);
+  const [activeStep, setActiveStep] = useState(0);
+  const [isActive, setIsActive] = useState(false);
 
   const books = data.genres.map(item => {
     return <li key={item.id}>{item.name}</li>;
   });
 
-  console.log(step);
+  console.log(activeStep);
   const nextStepHandler = e => {
     console.log("Next");
+    setActiveStep(prevActiveStep => prevActiveStep + 1);
   };
 
   const prevStepHandler = e => {
     console.log("Prev");
+    setActiveStep(prevActiveStep => prevActiveStep - 1);
   };
+
+  // const renderContent = () => {
+  //   switch (this.state.currentStep) {
+  //   switch (activeStep) {
+  //     case 0: return (<Template1 verify={this.verify} changeCurrentStep={setActiveStep} />)
+  //   }
+  // }
 
   return (
     <div className={classes.MainShelf}>
       <h1>Add book - New Book</h1>
-      <div style={{ display: "flex", justifyContent: "center" }}>
+      <div className={classes.MainContent}>
         <Genre />
         <SubGenre />
         <AddSubgenre />
